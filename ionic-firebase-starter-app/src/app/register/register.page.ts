e136625c901fd8bf91e5d335cfa4e2c5f83ad411
Alunos Eng <alunosengangular@gmail.com>
2019-05-30 17:34:32 -0300

    projeto login firebase ionic 4 completo

---
+++ ionic-firebase-starter-app/src/app/register/register.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'E-mail é obrigatório.' },
+     { type: 'pattern', message: 'Digite um e-mail válido.' }
+   ],
+   'password': [
+     { type: 'required', message: 'A Senha é obrigatória.' },
+     { type: 'minlength', message: 'A Senha deve ter pelo menos 5 caracteres.' }
+   ]
+  };
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+
+  tryRegister(value){
+    this.authService.doRegister(value)
+     .then(res => {
+       console.log(res);
+       this.errorMessage = "";
+       this.successMessage = "Sua conta foi criada com sucesso. Por favor efetue seu login.";
+     }, err => {
+       console.log(err);
+       this.errorMessage = err.message;
+       this.successMessage = "";
+     })
+  }
+
+  goLoginPage(){
+    this.router.navigate(["/login"]);
+  }
+
+}
---
+++ ionic-firebase-starter-app/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBD61W9y6v4ovLLL33caD-zBoq31imsl1Y",
+    authDomain: "app-alunos.firebaseapp.com",
+    databaseURL: "https://app-alunos.firebaseio.com",
+    projectId: "app-alunos",
+    storageBucket: "",
+    messagingSenderId: "205504004232",
+    appId: "1:205504004232:web:69e53d47e412a733"
+ }
+};
